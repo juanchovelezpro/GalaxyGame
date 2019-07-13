1bc57ae19368fc41b002a19f46f7982475f9808a
juanchovelezpro <velezolaya2012@hotmail.com>
2019-07-13 00:17:44 -0500

    Ventana del juego

--- GalaxyGame/src/launcher/Launcher.java
+++ GalaxyGame/src/launcher/Launcher.java
@@ -1,5 +1,12 @@
 package launcher;
 
+import vista.Game;
+
 public class Launcher {
 
+	
+	public static void main(String[] args) {
+		Game game = new Game();
+	}
+	
 }
--- GalaxyGame/src/vista/Game.java
+++ GalaxyGame/src/vista/Game.java
@@ -1,5 +1,27 @@
 package vista;
 
-public class Game {
+import javax.swing.*;
+import java.awt.*;
 
+
+public class Game extends JFrame{
+
+	public Game() {
+		
+	setTitle("Galaxy Game");
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+	setSize(1260,780);
+
+	
+	
+	setLocationRelativeTo(null);
+	
+	
+	setVisible(true);
+	
+	
+		
+	}
+	
+	
 }
